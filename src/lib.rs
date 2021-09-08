808cf5748668293b06f04ab3cf0af4755fb13a2c
Matt Moeller <ultamatt@gmail.com>
2021-09-08 15:06:09 -0700

    Adding in the secondary funciton to get mac by name

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use neon::prelude::*;
-use mac_address::get_mac_address;
+use mac_address::{ get_mac_address, mac_address_by_name };
 // 
 // fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
 //     Ok(cx.string("hello node"))
@@ -18,9 +18,24 @@ fn get_mac(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string(returnable))
 }
 
+fn get_mac_by_name(mut cx: FunctionContext) -> JsResult<JsString> {
+    let da_name = cx.argument::<JsString>(0)?.value(&mut cx);
+    let results = match mac_address_by_name(&da_name) {
+        Ok(Some(ma)) => Some(ma), 
+        Ok(None) => None, 
+        Err(e) => None,
+    };
+    let returnable = match results {
+        Some(ma) => ma.to_string(), 
+        None => String::from("No Mac or Invalid Interface"),
+    };
+    Ok(cx.string(returnable))
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     // cx.export_function("hello", hello)?;
+    cx.export_function("get_mac_by_name", get_mac_by_name);
     cx.export_function("get_mac", get_mac)?;
     Ok(())
 }
