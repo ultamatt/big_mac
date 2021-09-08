aa833af0326b153b72d34e285b08917a104636d5
Matt Moeller <ultamatt@gmail.com>
2021-09-08 15:32:16 -0700

    Default to 'nothing' mac

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use neon::prelude::*;
 use mac_address::{ get_mac_address, mac_address_by_name };
-// 
-// fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-//     Ok(cx.string("hello node"))
-// }
+const default_mac:& str = "00:00:00:00:00:00";
 
 fn get_mac(mut cx: FunctionContext) -> JsResult<JsString> {
     let results = match get_mac_address() {
@@ -11,11 +8,16 @@ fn get_mac(mut cx: FunctionContext) -> JsResult<JsString> {
         Ok(None) => None,
         Err(e) => None,
     };
-    let returnable = match results {
-        Some(ma) => ma.to_string(),
-        None => String::from("No Mac"),
+    
+    match results {
+        Some(ma) => {
+            return Ok(cx.string(ma.to_string()))
+        },
+        None => {
+            //panic!("No Mac dude");
+            return Ok(cx.string(& default_mac))
+        },
     };
-    Ok(cx.string(returnable))
 }
 
 fn get_mac_by_name(mut cx: FunctionContext) -> JsResult<JsString> {
@@ -25,16 +27,20 @@ fn get_mac_by_name(mut cx: FunctionContext) -> JsResult<JsString> {
         Ok(None) => None, 
         Err(e) => None,
     };
-    let returnable = match results {
-        Some(ma) => ma.to_string(), 
-        None => String::from("No Mac or Invalid Interface"),
+    
+    match results {
+        Some(ma) => {
+            return Ok(cx.string(ma.to_string()))
+        }, 
+        None => {
+            //panic!(String::from("No Mac or Invalid Interface"));
+            return Ok(cx.string(& default_mac))
+        }
     };
-    Ok(cx.string(returnable))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    // cx.export_function("hello", hello)?;
     cx.export_function("get_mac_by_name", get_mac_by_name);
     cx.export_function("get_mac", get_mac)?;
     Ok(())
